e495cbc106e3bcd191788a206ba3d86823c6d9cd
simplex3510 <leeyj6457@naver.com>
2023-07-21 01:03:12 +0900

    0.1.1Ver
    공공데이터 APIManager 작성
    대대적인 클래스 변경 작업 필요

--- Assets/Scripts/Stock.cs
+++ Assets/Scripts/Stock.cs
@@ -3,14 +3,6 @@ using System;
 using UnityEngine;
 using UnityEngine.UI;
 
-public class StockData : ScriptableObject
-{
-    
-
-    
-}
-
-
 public class Stock : MonoBehaviour
 {
     [Header("Components")]
